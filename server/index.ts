2b3cba02e402577412c03cf8b03ecb7756e621af
surendravarikallu <varikallusurendra@gmail.com>
2025-10-24 18:13:57 +0530

    fixed files to deploy in render

--- server/index.ts
+++ server/index.ts
@@ -99,7 +99,7 @@ app.use((req: Request, res: Response, next: NextFunction) => {
     // this serves both the API and the client.
     // It is the only port that is not firewalled.
     const port = parseInt(process.env.PORT || '5000', 10);
-    const host = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
+    const host = '0.0.0.0';
     
     server.listen({
       port,
